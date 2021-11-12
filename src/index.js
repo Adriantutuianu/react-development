e6c39558dd295e9e1870f784bb150beb2c5f05a0
Adrian <adriantutuianu01@gmail.com>
2021-11-12 23:48:20 +0000

    Vertical spacing JSX

--- src/index.js
+++ src/index.js
@@ -15,4 +15,11 @@ ReactDOM.render(
 // onchange attribute in HTML will be onChange in JavaScript
 // onclick attribute in HTML will be onClick in JavaScript
 
+// To fix the problem of vertical spacing, explicitly add white space as follows.
+//  <p>
+// 	Hello
+// 	{ ‘ ‘ } JSX
+// </p>
+// This would become Hello JSX.
+
 reportWebVitals();
