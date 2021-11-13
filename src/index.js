9922672c6442c587affa13de85c6f7432b907ca5
Adrian <adriantutuianu01@gmail.com>
2021-11-13 23:41:46 +0000

    Props definition

--- src/index.js
+++ src/index.js
@@ -12,10 +12,13 @@ ReactDOM.render(
 );
 
 /*
-React elements have four major properties namely type, key, ref, and props.
-type, this will be a string that represents a HTML tag or referencing a React component.
-key, this property will hold a string that helps in uniquely identifying a React element.
-ref, this will be a reference to the instance of the React component and props will refer to the properties of the object.
+Props are read-only and immutable. They are used in customizing the React component while creating it.
+If you want some immutable data in your React Component, add the props to the ReactDOM.render() method. Then use it inside the component.
+Props make the React component more dynamic and more reusable. It is advised to use the camelCase formatting while you create your own React props.
+Props are a common way of passing the data from the parent to the child component.
+With props, you can pass the custom data to your React component.
+The major purpose of the props in React is to provide the component diverse functionalities.
+Props will also allow you to trigger the ‘state’ changes. We will later learn about ‘state’ in detail.
 */
 
 reportWebVitals();
