0559eb4a8320e466f95e91bc6b25b6bc77b5bf43
Adrian <adriantutuianu01@gmail.com>
2021-11-13 23:09:16 +0000

    Create an element

--- src/index.js
+++ src/index.js
@@ -4,17 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const title = React.createElement("h1", {}, "First React Element");
+React.createElement();
 
-/*
-Stateful React components are always Class Components!
-These components have a state. The state should be initialized in the constructor.
-You shouldn't stop with just this initialization of the state. That will not help you create an interactive user interface.
-You need to update the state. Updating the states will allow you to create a highly responsive and interactive React JS application.*/
+ReactDOM.render(title, document.getElementById("root"));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 reportWebVitals();
