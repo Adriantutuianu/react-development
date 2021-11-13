75927ec762cadb78addf243ff83a9550ebdffb94
Adrian <adriantutuianu01@gmail.com>
2021-11-13 23:29:58 +0000

    Elements properties

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const title = React.createElement("h1", {}, "First React Element");
-React.createElement();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(title, document.getElementById("root"));
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+/*
+React elements have four major properties namely type, key, ref, and props.
+type, this will be a string that represents a HTML tag or referencing a React component.
+key, this property will hold a string that helps in uniquely identifying a React element.
+ref, this will be a reference to the instance of the React component and props will refer to the properties of the object.
+*/
 
 reportWebVitals();
