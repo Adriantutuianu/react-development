50c71e43d5e6226d403c648c60aa27a354450e6b
Adrian <adriantutuianu01@gmail.com>
2021-11-14 16:18:32 +0000

    setState()

--- src/index.js
+++ src/index.js
@@ -12,14 +12,10 @@ ReactDOM.render(
 );
 
 /*
-‘State’ is private to a React component!. In this aspect, props and state differ.
-The value of the ‘State’ is solely controlled by only the component.
-We know that React is immutable. So, the state should never be changed directly, changes must be made only to the copy of the current version of the state.
-This will allow you to review the state at a different point in time. This will help you while building huge React applications.
-The state can thus be defined as a set of observable properties which control the behavior of the React component.
-The information held by the state object may change over the lifetime of the React component.
-States can be used only in class-based components! You cannot use states in functional React components.
-
+setState() is the legit way to update the state of the React Component.
+The state of the component needs to be updated as a response to the event handlers.
+setState() adds the changes to the state of the component.
+It then tells React that the component and its children need to be re-rendered, now with the updated state.
 */
 
 reportWebVitals();
