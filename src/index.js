f990a2da4905a5b43454a66ae4920e4753939bf6
Adrian <adriantutuianu01@gmail.com>
2021-11-14 16:25:24 +0000

    Props vs state

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ Props set by the parent component are then used by the child components.
 When the user presses a key or performs a mouse click, the state of the component is changed.
 In a React component, the props are the variables which get passed to it by its parent Components.
 But State is still the variable that gets directly initialized and managed by the React component.
-
+States refer to the local state of the React component. The value of the states can be changed and updated.
+Props are immutable and can be used in functional or class-based components.
+Have a minimum number of stateful components. This will ensure a performance boost.
 
 
 
