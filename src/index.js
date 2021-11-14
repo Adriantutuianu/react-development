a5d2f068d55f9f85c2d271569cbe71977b3a475b
Adrian <adriantutuianu01@gmail.com>
2021-11-14 16:13:32 +0000

    State

--- src/index.js
+++ src/index.js
@@ -12,13 +12,14 @@ ReactDOM.render(
 );
 
 /*
-A good strategy to pass all the props from the parent component to the child component is through the use of JavaScript spread operator.
-This spread operator is a powerful feature in React JS. This feature comes from JavaScript.
-The props spreading is used in spreading a whole object with the key value pairs down to the child React component.
-The spread operator will have the same effect as passing each value of the object on its own to the component.
-The syntax of the spread operator is easy and is less prone to errors.
-In addition, the JavaScript spread operator offers a great level of flexibility to React JS.
-You need not change the name of the props or add the props in the intermediate component.
+‘State’ is private to a React component!. In this aspect, props and state differ.
+The value of the ‘State’ is solely controlled by only the component.
+We know that React is immutable. So, the state should never be changed directly, changes must be made only to the copy of the current version of the state.
+This will allow you to review the state at a different point in time. This will help you while building huge React applications.
+The state can thus be defined as a set of observable properties which control the behavior of the React component.
+The information held by the state object may change over the lifetime of the React component.
+States can be used only in class-based components! You cannot use states in functional React components.
+
 */
 
 reportWebVitals();
