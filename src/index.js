76eb508a387b89e0859e1bc988fb14d6fdc69f2b
Adrian <adriantutuianu01@gmail.com>
2021-11-15 22:41:23 +0000

    Validating the forms

--- src/index.js
+++ src/index.js
@@ -16,6 +16,14 @@ Of the few HTML elements, Forms are the most interactive element by default.
 In real-time forms are designed to allow the users to interact well with a web page.
 Forms are commonly used in login and registration windows, during the checkout and shopping cart screen of e-commerce websites.
 Contact forms are also common. These forms are present in almost all websites.
+Forms are used in the webpages for the user to enter the details.
+Once entered and submitted, the form details are sent to the server for processing.
+The data entered in the form needs to be in the perfect format.
+In addition, some fields need to be compulsorily filled by the user.
+Some mandatory form fields are username and password details.
+Validating the forms can be handled using the handleChange method.
+You will access the old and the new value of the state.
+Then you need to check the new value and if found invalid, reject the updated new value. In addition, communicate this scenario to the user.
 
 
 */
