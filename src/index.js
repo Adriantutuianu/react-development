969a6289d182c2e5649840c95baf6de48bab3237
Adrian <adriantutuianu01@gmail.com>
2021-11-16 21:38:24 +0000

    Event binding

--- src/index.js
+++ src/index.js
@@ -19,6 +19,9 @@ The performance is achieved automatically using the Event Delegation.
 Event delegation is a technique for listening to events where the parent element is delegated as the listener.
 The parent element is chosen as the listener for all the events that happen within it. A single event listener is attached to the root of the document.
 
+What is Event Binding? What are the different ways of performing the event binding?
+Event binding tells the browser a particular function needs to be called when the event gets triggered.
+When we don't bind the event handler method with the component instance, the context will be lost.
 
 
 */
