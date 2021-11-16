87c72f7b71bfd59a5c6b0b531ad098385d1ae948
Adrian <adriantutuianu01@gmail.com>
2021-11-16 21:35:48 +0000

    Synthetic event system

--- src/index.js
+++ src/index.js
@@ -12,16 +12,12 @@ ReactDOM.render(
 );
 
 /*
-Events are things or actions that occur in your system.
-You need your system to respond to the events in a specific way.
-Events in React JS are like the events in Javascript! The only difference is their syntax.
-Yes, React JS offers a more simple and elegant syntax!
-Different types of event bindings are provided.
-The event handlers help in determining what actions should be taken when the event gets fired.
-The events can be a normal mouse click, key press, window resize or a change in the text input.
-As a response to an event, show an alert box to the user.
-The responses can be navigation to another window, the change in the display or occurrence of the new screen or images.
-
+React JS implements a synthetic event system.
+This event system brings a high level of consistency.
+You can find different events across different browsers and platforms.
+We need a normalized event system to design the event handlers.
+We need a normalized event system to design the event handlers.
+The synthetic event offers consistency. It is achieved by normalizing all the events that have the same properties.
 */
 
 reportWebVitals();
