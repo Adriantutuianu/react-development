58d009db2696b140db07afc4635b1df835f9a65a
Adrian <adriantutuianu01@gmail.com>
2021-11-16 21:34:03 +0000

    Event handlers

--- src/index.js
+++ src/index.js
@@ -17,7 +17,10 @@ You need your system to respond to the events in a specific way.
 Events in React JS are like the events in Javascript! The only difference is their syntax.
 Yes, React JS offers a more simple and elegant syntax!
 Different types of event bindings are provided.
-
+The event handlers help in determining what actions should be taken when the event gets fired.
+The events can be a normal mouse click, key press, window resize or a change in the text input.
+As a response to an event, show an alert box to the user.
+The responses can be navigation to another window, the change in the display or occurrence of the new screen or images.
 
 */
 
