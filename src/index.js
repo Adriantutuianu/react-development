4fd5e0d9ba66ea114c0a2d9f4f88d0863540ab46
Adrian <adriantutuianu01@gmail.com>
2021-11-16 21:33:10 +0000

    Events

--- src/index.js
+++ src/index.js
@@ -12,22 +12,12 @@ ReactDOM.render(
 );
 
 /*
-Of the few HTML elements, Forms are the most interactive element by default.
-In real-time forms are designed to allow the users to interact well with a web page.
-Forms are commonly used in login and registration windows, during the checkout and shopping cart screen of e-commerce websites.
-Contact forms are also common. These forms are present in almost all websites.
-Forms are used in the webpages for the user to enter the details.
-Once entered and submitted, the form details are sent to the server for processing.
-The data entered in the form needs to be in the perfect format.
-In addition, some fields need to be compulsorily filled by the user.
-Some mandatory form fields are username and password details.
-Validating the forms can be handled using the handleChange method.
-You will access the old and the new value of the state.
-Then you need to check the new value and if found invalid, reject the updated new value. In addition, communicate this scenario to the user.
-onChange: This event gets fired whenever there is some change in any of the input element on the form.
-onChange is the widely used event during the validation of the form.
-onInput: This event gets fired automatically for every change that happens in the <input> elements and the <textarea>
-onInput event is not recommended to be used! Puzzled?
+Events are things or actions that occur in your system.
+You need your system to respond to the events in a specific way.
+Events in React JS are like the events in Javascript! The only difference is their syntax.
+Yes, React JS offers a more simple and elegant syntax!
+Different types of event bindings are provided.
+
 
 */
 
